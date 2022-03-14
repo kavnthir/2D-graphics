388b6e357520d43559d4ab5284cd13635599f79a
kavin t <ksbkthirukonda@gmail.com>
2022-03-14 18:35:52 -0400

    finished raycast

--- raytracing/RayWidget.cpp
+++ raytracing/RayWidget.cpp
@@ -1,7 +1,27 @@
 #include "RayWidget.h"
 
+bool onSegment(QPointF p, QPointF q, QPointF r) {
+    if (q.x() <= std::max(p.x(), r.x()) && q.x() >= std::min(p.x(), r.x()) &&
+        q.y() <= std::max(p.y(), r.y()) && q.y() >= std::min(p.y(), r.y())) return true;
+    return false;
+}
+
 RayWidget::RayWidget(QWidget * parent): QWidget(parent){
+	QDesktopWidget desktop;
+	int width = desktop.width() * 0.7;
+	int height = desktop.height() * 0.7;
+
 	pos = QPointF();
+
+	for(int i = 0; i < WALL_COUNT; i++){
+		int start_x = rand() % width;
+		int start_y = rand() % height;
+		int end_x = rand() % width;
+		int end_y = rand() % height;
+
+		walls.push_back(QLine(QPoint(start_x, start_y), QPoint(end_x, end_y)));
+	}
+
 }
 
 void RayWidget::updatePos(){
@@ -11,9 +31,6 @@ void RayWidget::updatePos(){
 	pos = QCursor::pos() - frameGeometry().topLeft();
 
 	this->update();
-
-	// std::cout << "x: " << frameGeometry().x() << ", y: " << frameGeometry().y() << '\n';
-	//std::cout << "x: " << pos.x() << ", y: " << pos.y() << '\n';
 }
 
 void RayWidget::paintEvent(QPaintEvent *event){
@@ -23,13 +40,57 @@ void RayWidget::paintEvent(QPaintEvent *event){
 	QPen pen(Qt::white);
 	pen.setWidthF(1);
 	painter.setPen(pen);
-	painter.translate(pos);
+	QPen penWalls(Qt::gray);
+	penWalls.setWidthF(5);
 
-	double stepSize = 3;
-	for(double i = 0; i < 360; i+=stepSize){
-		painter.drawLine(QPoint(0, 0), QPoint(0, 0) + QPoint(0, - desktop.width()));
-		painter.rotate(stepSize);
+	for(double i = 0; i < 360; i+=.5){
 
+		double x = desktop.width() * 2 * cos(i); 
+		double y = desktop.width() * 2 * sin(i); 
+
+		QLineF ray(pos, QPointF(x, y));	
+		QPointF intersect;
+
+		for(int j = 0;  j < WALL_COUNT; j++){
+			double x1 = ray.x1();
+			double x2 = ray.x2();
+			double x3 = walls[j].x1();
+			double x4 = walls[j].x2();
+			double y1 = ray.y1();
+			double y2 = ray.y2();
+			double y3 = walls[j].y1();
+			double y4 = walls[j].y2();
+
+			double D = ( x1 - x2 )*( y3 - y4 ) - ( y1 - y2 )*( x3 - x4 );	
+
+			double xN = (x1*y2 - y1*x2)*(x3 - x4) - (x1 -x2)*(x3*y4 - y3*x4);
+			double yN = (x1*y2 - y1*x2)*(y3 - y4) - (y1 -y2)*(x3*y4 - y3*x4);
+
+			QPointF temp = QPointF( xN / D, yN / D );
+
+			if(!onSegment(walls[j].p1(), temp, walls[j].p2()) || !onSegment(pos, temp, QPointF(x,y))){
+				continue;
+			}
+
+			QLineF calc(intersect, pos);
+			QLineF calc2(temp, pos);
+			if(intersect.isNull() || calc.length() > calc2.length()){
+				intersect = temp;
+			}
+
+		}
+
+		if(!intersect.isNull()){
+			painter.drawLine(pos, intersect);
+			continue;
+		}
+		painter.drawLine(pos, QPointF(x, y));
+	}
+
+	painter.setPen(penWalls);
+	painter.resetTransform();
+	for(int i = 0; i < WALL_COUNT; i++){
+		painter.drawLine(walls[i]);
 	}
 	
 }
--- raytracing/RayWidget.h
+++ raytracing/RayWidget.h
@@ -6,7 +6,14 @@
 #include <QWidget>
 #include <QPainter>
 #include <QCursor>
+#include <QLine>
+
 #include <iostream>
+#include <vector>
+#include <math.h>
+#include <cmath>
+
+#define WALL_COUNT 10
 
 class RayWidget: public QWidget {
   Q_OBJECT
@@ -23,6 +30,7 @@ public slots:
 private:
 	QPointF pos;
 	QPainter painter;
+	std::vector<QLine> walls;
 };
 
 #endif
